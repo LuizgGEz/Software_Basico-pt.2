12731b69c0f15c0bb9244c2fe4aafe5721fe1d17
Luiz Guilherme Vilas Boas da Costa Martins <133516724+LuizgGEz@users.noreply.github.com>
2023-05-20 20:44:55 -0300

    Update lista7_ex7.py

--- lista7_ex7.py
+++ lista7_ex7.py
@@ -1,5 +1,5 @@
 def numero(valor):
-    if (valor/2>1) or (valor/3>1) or (valor/5>1) or (valor/7>1) :
+    if (valor/2==int) or (valor/3==int) or (valor/5==int) or (valor/7==int) :
         return print(0)
     elif (valor/1==valor) and (valor/valor==1):
         return print(1)
